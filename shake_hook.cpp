847ebd52e5b68cd526923e337434c8c1f9f301d3
QianfoamsmallXianxian <141899410+QianfoamsmallXianxian@users.noreply.github.com>
2026-09-26 23:11:20 +0800

    chore: remove dead C4 mod file shake_hook.cpp

--- shake_hook.cpp
+++
@@ -1,9 +0,0 @@
-#include "c4_fx_mod.h"
-void __MsgFunc_Shake(const char *pszName, int iSize, void *pbuf) {
-    BEGIN_READ(pbuf, iSize);
-    float amplitude = READ_SHORT(); float duration = READ_SHORT(); float frequency = READ_SHORT();
-    float scale = C4FX_GetShakeScale();
-    if (scale < 0.0f) scale = 0.0f; if (scale > 5.0f) scale = 5.0f;
-    amplitude *= scale;
-    gEngfuncs.pfnV_CalcShake(); gEngfuncs.pfnV_ApplyShake(amplitude, duration, frequency);
-}
